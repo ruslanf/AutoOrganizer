aaf4d4ea431e654774bfd22e0d0ce12ac39712f2
Ruslan <r.freydlin@alpinadigital.ru>
2024-06-17 21:10:49 +0300

    Fixed for showing when progress greater than zero

--- app/src/main/java/org/bz/autoorganizer/ui/base/LoadingIndicator.kt
+++ app/src/main/java/org/bz/autoorganizer/ui/base/LoadingIndicator.kt
@@ -1,49 +1,24 @@
 package org.bz.autoorganizer.ui.base
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import kotlinx.coroutines.flow.Flow
 
+/**
+ * Composable function for showing circular progressbar
+ * when progress value greater than 0
+ */
 @Composable
 fun LoadingIndicator(
-    progress: Flow<Int>
+    modifier: Modifier = Modifier,
+    progress: Int = 0
 ) {
-    val count by progress.collectAsState(initial = 0)
-
-    ConstraintLayout(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        val (topText) = createRefs()
-        if (count > 0) CircularProgressIndicator(
-            modifier = Modifier
-                .width(32.dp)
-                .constrainAs(topText) {
-                    centerHorizontallyTo(parent)
-                    centerVerticallyTo(parent)
-                },
-            color = MaterialTheme.colorScheme.secondary,
-            trackColor = MaterialTheme.colorScheme.surface
-        )
-    }
-}
-
-@Composable
-fun LoadingIndicator(
-    progress: Flow<Int>,
-    modifier: Modifier = Modifier
-) {
-    val count by progress.collectAsState(initial = 0)
-
-    if (count > 0) CircularProgressIndicator(
-        modifier = modifier.width(32.dp),
+    if (progress > 0) CircularProgressIndicator(
+        modifier = modifier
+            .width(32.dp),
         color = MaterialTheme.colorScheme.secondary,
         trackColor = MaterialTheme.colorScheme.surface
     )
