c839e927e628d948631458af18c9abd2e3f1aec4
Ruslan <r.freydlin@alpinadigital.ru>
2024-06-17 21:07:15 +0300

    Field invisible when it's disabled

--- app/src/main/java/org/bz/autoorganizer/ui/profile/auto/AutoModelFields.kt
+++ app/src/main/java/org/bz/autoorganizer/ui/profile/auto/AutoModelFields.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
@@ -36,7 +37,8 @@ fun AutoModelField(
             .fillMaxWidth()
             .onGloballyPositioned { layoutCoordinates ->
                 modelSize = layoutCoordinates.size.toSize()
-            },
+            }
+            .alpha(if (isEnabled) 1f else 0f),
         value = selectedModel,
         onValueChange = { model = it },
         label = {
